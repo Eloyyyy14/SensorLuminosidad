5332fe9c62923ea633285010d2bc244027f6e20e
Eloyyyy14 <32665791+Eloyyyy14@users.noreply.github.com>
2018-01-17 13:21:04 +0100

    Initial commit

---
+++ app/src/main/java/com/example/elo14/sensorluminosidad/MainActivity.java
@@ -0,0 +1,67 @@
+package com.example.elo14.sensorluminosidad;
+
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SensorManager sensorManager;
+    private Sensor sensorLuz;
+    private SensorEventListener luzEventListener;
+    private View layoutRaiz;
+    private float maxValue;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        layoutRaiz=findViewById(R.id.layoutRaiz);
+        sensorManager=(SensorManager)getSystemService(SENSOR_SERVICE);
+        sensorLuz=sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+
+        //Comprobacion de que tiene sensor de luz
+        if(sensorLuz==null){
+            Toast.makeText(this, "Su dispositivo no tiene sensor de luminosidad :(", Toast.LENGTH_LONG).show();
+            finish();
+        }
+
+        maxValue=sensorLuz.getMaximumRange();
+
+        luzEventListener=new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent event) {
+                float valor=event.values[0];
+                getSupportActionBar().setTitle("Luminosidad: "+valor+" lx");
+
+                int nuevoValor=(int)(255*valor/maxValue);
+                layoutRaiz.setBackgroundColor(Color.rgb(nuevoValor, nuevoValor, nuevoValor));
+
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+            }
+        };
+
+    }
+    @Override
+    public void onResume(){
+        super.onResume();
+        sensorManager.registerListener(luzEventListener, sensorLuz, sensorManager.SENSOR_DELAY_FASTEST);
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        sensorManager.unregisterListener(luzEventListener);
+    }
+}
